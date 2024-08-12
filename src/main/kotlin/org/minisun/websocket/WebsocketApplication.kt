c2fd47be96505a8b9715fab03e96f9237bc5ebab
minisun <minisundev@gmail.com>
2024-08-12 09:46:08 +0900

    feat: init commit

---
+++ src/main/kotlin/org/minisun/websocket/WebsocketApplication.kt
@@ -0,0 +1,11 @@
+package org.minisun.websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketApplication>(*args)
+}
