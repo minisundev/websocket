640ee49ee9478c380813c315e8d86afa7496743f
minisun <minisundev@gmail.com>
2024-08-24 09:36:51 +0900

    feat: Add CORS exception

--- src/main/kotlin/org/minisun/websocket/config/WebSocketConfig.kt
+++ src/main/kotlin/org/minisun/websocket/config/WebSocketConfig.kt
@@ -5,18 +5,25 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration
 
 @Configuration
 @EnableWebSocketMessageBroker
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
-    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
-        registry.enableSimpleBroker("/topic","/queue")
-        registry.setApplicationDestinationPrefixes("/app")
-    }
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/chatting")
-            .setAllowedOriginPatterns("*")//Origins -> Origin Patterns
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins("http://localhost:63342")
             .withSockJS()
+    }
+
+    override fun configureMessageBroker(config: MessageBrokerRegistry) {
+        config.setApplicationDestinationPrefixes("/app")
+        config.enableSimpleBroker("/topic", "/queue")
+    }
+
+    override fun configureWebSocketTransport(registry: WebSocketTransportRegistration) {
+        registry.setMessageSizeLimit(4 * 8192)
+        registry.setTimeToFirstMessage(30000)
     }
 }
